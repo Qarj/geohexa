f23ef7bc781dce3ec08038b6450906a4cac64614
Tim Buckland <tim.buckland@gmail.com>
2022-07-11 22:39:51 +0100

    feat: reformat js files, create npm package

--- geohexa.js
+++ geohexa.js
@@ -1,12 +1,11 @@
-var BASE_36_DIGITS_OUTPUT = "0123456789abcdefghijkLmnopqrstuvwxyz";
+var BASE_36_DIGITS_OUTPUT = '0123456789abcdefghijkLmnopqrstuvwxyz';
 var BASE_36_DIGITS = BASE_36_DIGITS_OUTPUT.toLowerCase();
 
-Math.toRadians = function(degrees) {
-  return degrees * Math.PI / 180;
+Math.toRadians = function (degrees) {
+    return (degrees * Math.PI) / 180;
 };
 
-function distance (lat1, lon1, lat2, lon2) {
-
+function distance(lat1, lon1, lat2, lon2) {
     var latDistance = Math.toRadians(lat2 - lat1);
     var lonDistance = Math.toRadians(lon2 - lon1);
 
@@ -38,14 +37,14 @@ function prepareLon(lon) {
 
 function compress(num, cellSize) {
     var cell = Math.floor(num / cellSize);
-    var remainder = num - ( cell * cellSize);
+    var remainder = num - cell * cellSize;
     var base36 = BASE_36_DIGITS_OUTPUT.charAt(cell);
 
     //if (VERBOSE) {
     //    System.out.println ("base36 digit: " + base36 + "(" + cell + ")  cell_size_degrees[" + cell_size + "]   fragment[" + num + "]  remainder_to_go[" + remainder + "]");
     //}
 
-    return {base36: base36, remainder: remainder};
+    return { base36: base36, remainder: remainder };
 }
 
 function latLonToGeohexa(lat, lon, accuracy) {
@@ -57,24 +56,25 @@ function latLonToGeohexa(lat, lon, accuracy) {
     lon = parseFloat(lon);
     accuracy = parseFloat(accuracy);
 
-    if (accuracy < 0.00000001) { // need to be sensible about the accuracy to prevent division by zero
+    if (accuracy < 0.00000001) {
+        // need to be sensible about the accuracy to prevent division by zero
         accuracy = 0.00000001;
     }
-    var geohexa = "";
-    var allLatgeo = "";
+    var geohexa = '';
+    var allLatgeo = '';
     var latFragment = prepareLat(lat);
     var lonFragment = prepareLon(lon);
     var latUnit = 180;
     var lonUnit = 360;
-    
+
     //alert ("Preparing lat " + prepareLat(lat));
-    
+
     //alert ("latFragment, lonFragment: ", latFragment + ", " + lonFragment);
 
     // null is a valid geohexa - represents 0, 0
     var error = distance(lat, lon, 0, 0);
     if (error < accuracy) {
-        return "";
+        return '';
     }
 
     var latCellSize = 0;
@@ -120,11 +120,10 @@ function latLonToGeohexa(lat, lon, accuracy) {
         if (error < accuracy) {
             break;
         }
-        
+
         if (count > 15) {
             break;
         }
-        
     }
 
     return geohexa;
@@ -143,7 +142,8 @@ function geohexaToLatLon(hexa) {
         var d = hexa[i].toLowerCase();
         digitNum = digitNum + 1;
         var remainder = digitNum % 2;
-        if (remainder == 1 ) { // we have a lon geohexa digit
+        if (remainder == 1) {
+            // we have a lon geohexa digit
             lon = lon + (lonWidth / 36) * BASE_36_DIGITS.indexOf(d);
             lonWidth = lonWidth / 36;
         } else {
@@ -153,14 +153,14 @@ function geohexaToLatLon(hexa) {
     }
 
     // calculate mid point of cell
-    lat = lat - 90  + latHeight / 2;
+    lat = lat - 90 + latHeight / 2;
     lon = lon - 180 + lonWidth / 2;
 
     //if (VERBOSE) {
     //    System.out.println(hexa + " as lat, lon " + lat + " " + lon);
     //}
-    
-    return {lat: lat, lon: lon};
+
+    return { lat: lat, lon: lon };
 }
 
 function inRange(hexa) {
@@ -176,102 +176,102 @@ function inRange(hexa) {
 function updateLatLon() {
     var lat = updateLat();
     var lon = updateLon();
-    
-    var eOGeohexa = document.getElementById("oGeohexa");
-    if (! (lat.ok && lon.ok) ) {
-        eOGeohexa.textContent = "";
+
+    var eOGeohexa = document.getElementById('oGeohexa');
+    if (!(lat.ok && lon.ok)) {
+        eOGeohexa.textContent = '';
         //alert("lat or lon is no ok");
         return;
     }
-    
-    var eAccuracy  = document.getElementById("accuracy");
+
+    var eAccuracy = document.getElementById('accuracy');
     var accuracy = parseFloat(eAccuracy.value);
 
-    var eOAccuracy = document.getElementById("oAccuracy");
-    if ( isNaN(accuracy) ) {
-        eOAccuracy.textContent = "default is 3";
+    var eOAccuracy = document.getElementById('oAccuracy');
+    if (isNaN(accuracy)) {
+        eOAccuracy.textContent = 'default is 3';
         accuracy = 3;
     } else {
-        eOAccuracy.textContent = "";
+        eOAccuracy.textContent = '';
     }
 
     //alert("calculating geohexa for lat " + lat.value + " and lon " + lon.value );
     var geohexa = latLonToGeohexa(lat.value, lon.value, accuracy);
 
-    if (geohexa == "") {
-        geohexa = "[null geohexa]";
+    if (geohexa == '') {
+        geohexa = '[null geohexa]';
     }
     eOGeohexa.textContent = geohexa;
 }
 
 function updateLat() {
-    var eLat = document.getElementById("lat");
+    var eLat = document.getElementById('lat');
     var lat = parseFloat(eLat.value);
 
-    if ( isNaN(lat) ) {
-        return {ok: false};
+    if (isNaN(lat)) {
+        return { ok: false };
     }
-    
-    var eLatError = document.getElementById("latError");
 
-    if ( lat >= -90 && lat <= 90 ) {
-        eLat.style.background = "";
-        eLatError.textContent = "";
+    var eLatError = document.getElementById('latError');
+
+    if (lat >= -90 && lat <= 90) {
+        eLat.style.background = '';
+        eLatError.textContent = '';
         //alert("lat is ok");
     } else {
-        eLat.style.background = "yellow";
-        eLatError.textContent = "The latitude must be from -90 to 90";
+        eLat.style.background = 'yellow';
+        eLatError.textContent = 'The latitude must be from -90 to 90';
         //alert("lat is not ok");
-        return {ok: false};
+        return { ok: false };
     }
-    
-    return {ok: true, value: lat};
+
+    return { ok: true, value: lat };
 }
 
 function updateLon() {
-    var eLon = document.getElementById("lon");
+    var eLon = document.getElementById('lon');
     var lon = parseFloat(eLon.value);
 
-    if ( isNaN(lon) ) {
-        return {ok: false};
+    if (isNaN(lon)) {
+        return { ok: false };
     }
 
-    var eLonError = document.getElementById("lonError");
+    var eLonError = document.getElementById('lonError');
 
     if (lon >= -180 && lon <= 180) {
-        eLon.style.background = "";
-        eLonError.textContent = "";
+        eLon.style.background = '';
+        eLonError.textContent = '';
         //alert("lon is ok - value [" + lon + "]");
     } else {
-        eLon.style.background = "yellow";
-        eLonError.textContent = "The longitude must be from -180 to 180";
+        eLon.style.background = 'yellow';
+        eLonError.textContent = 'The longitude must be from -180 to 180';
         //alert("lon is not ok");
-        return {ok: false};
+        return { ok: false };
     }
 
-    return {ok: true, value: lon};
+    return { ok: true, value: lon };
 }
 
 function updateGeohexa() {
-    var eHexaError = document.getElementById("hexaError");
+    var eHexaError = document.getElementById('hexaError');
 
-    var eHexa = document.getElementById("geohexa");
+    var eHexa = document.getElementById('geohexa');
     var hexa = eHexa.value.toLowerCase();
 
-    var eOLat = document.getElementById("oLat");
-    var eOLon = document.getElementById("oLon");
+    var eOLat = document.getElementById('oLat');
+    var eOLon = document.getElementById('oLon');
 
     if (inRange(hexa)) {
-        eHexa.style.background = "";
-        eHexaError.textContent = "";
+        eHexa.style.background = '';
+        eHexaError.textContent = '';
     } else {
-        eHexa.style.background = "yellow";
-        eOLat.textContent = "";
-        eOLon.textContent = "";
-        eHexaError.textContent = "The geohexa must only use the digits 0-9 and a-z";
+        eHexa.style.background = 'yellow';
+        eOLat.textContent = '';
+        eOLon.textContent = '';
+        eHexaError.textContent = 'The geohexa must only use the digits 0-9 and a-z';
         return;
     }
-    
+
     var coord = geohexaToLatLon(hexa);
     eOLat.textContent = coord.lat;
     eOLon.textContent = coord.lon;
--- karma.conf.js
+++ karma.conf.js
@@ -1,70 +1,53 @@
 // Karma configuration
 // Generated on Wed Nov 15 2017 21:27:21 GMT+0000 (GMT Standard Time)
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    config.set({
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['qunit'],
 
+        // list of files / patterns to load in the browser
+        files: ['geohexa.js', 'geohexaTests.js'],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['qunit'],
+        // list of files to exclude
+        exclude: [],
 
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {},
 
-    // list of files / patterns to load in the browser
-    files: [
-      'geohexa.js',
-      'geohexaTests.js'
-    ],
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
 
+        // web server port
+        port: 9876,
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome'],
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
 
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity,
+    });
+};
